ad19f0de7543ae3b2f0a6a1a82ec7b156f1f48c6
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-10 19:59:24 -0700

    reg

--- login_registration/login_reg/models.py
+++ login_registration/login_reg/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from __future__ import unicode_literals
+# Create your models here.\
+class UserManager(models.Manager):
+    def create_validator(self, reqPost):   
+        errors = {}
+        if len(reqPost['user_name'])
+        return errors
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.CharField(max_length=50)
+    password = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
--- login_registration/login_reg/views.py
+++ login_registration/login_reg/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import User
+import bcrypt
+from django.contrib import messages
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method =="POST":
+        errors = User.objects.create_validator(request.POST)
+        if len(errors)>0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            pass
+            #Create an account
